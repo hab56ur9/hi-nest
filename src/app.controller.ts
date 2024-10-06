31ba6dcf7d546576e16c61efe9d3512ce8330c1c
hab56ur9 <hab5bur9@gmail.com>
2024-10-06 16:10:55 +0900

    1.1 Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // server entry point
   constructor(private readonly appService: AppService) {}
 
+  // express의 router 설정을 해줄 필요가 없게됨
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/hello")
+  sayHello(): string {
+    return "Hello everyone";
+  }
+  @Post("/hello2")
+  sayHello2(): string {
+    return "Hello everyone";
   }
 }
