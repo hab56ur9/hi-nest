64f434868cfebf7d4b93d743990b3769b270530c
hab56ur9 <hab5bur9@gmail.com>
2024-10-06 16:28:01 +0900

    1.2 Services : 연습을 위해 main과 module을 제외한 controller, services 삭제, 여기서부터 동작 안됨

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 // 데코레이터 AppModule은 비어있는 class인데, 데코레이터에 정의된 내용을 가지고 있음.
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
