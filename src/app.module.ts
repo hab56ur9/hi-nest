99f307cd1ecfd93cc2401ad8089d9130d09a45cc
hab56ur9 <hab5bur9@gmail.com>
2024-10-07 13:00:21 +0900

    movies 서비스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 
 // 데코레이터 AppModule은 비어있는 class인데, 데코레이터에 정의된 내용을 가지고 있음.
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
