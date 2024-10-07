0b00141d2b075923f17f6630845bd4547cdeafa8
hab56ur9 <hab5bur9@gmail.com>
2024-10-07 12:44:07 +0900

    2.1 More Routes: @Query, @Body등 데코레이터 추가사용, :id요소를 가지는 uri는 가장 아래에 배치해야함

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 //controller의 entry point, 이 controller의 url들은 /controller 하위에 정의된다.
 @Controller('movies')
@@ -8,14 +8,22 @@ export class MoviesController {
     getAll(){
         return "This will return all movies";
     }
+    // search같은 하위 주소를 직접 지정하는 요청이 /:id
+    @Get("search")
+    search(@Query("year") searchingYear:string){
+        return `We are searching for a movie made after ${searchingYear}`;
+    }
     // "/movies/1"
     // get의 id와 param의 id는 같아야하지만 이걸 저장할 이름은 달라도된다
     @Get("/:id")
     getOne(@Param("id") number:string ){
         return `This will return one movie with the id : ${number}`;
     }
+
+    // HTTP 요청 body data를 @Body로 가져올 수 있음.
     @Post("/:id")
-    create(){
+    create(@Body() movieData){
+        console.log(movieData);
         return "This will create a movie";
     }
     @Delete("/:id")
@@ -26,7 +34,12 @@ export class MoviesController {
     // @Put 모든 리소스를 업데이트함 
     // @Patch 리소스의 일부부만 업데이트해줌
     @Patch("/:id")
-    patchMovie(@Param('id') movieId:string){
+    patchMovie(@Param('id') movieId:string, @Body() updateData){
+        return { 
+            updatedMovie: movieId,// 별도의 작업 없이 json으로 자동 파싱됨, 개꿀
+            ...updateData, // ... 문법은 json 이어 붙이기 용도
+        }
         return `This will patch a movie with the id : ${movieId}`;
     }
+
 }
