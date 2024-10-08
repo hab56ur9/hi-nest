27999817e55b28b829649f180f0f57e19e86d93c
hab56ur9 <hab5bur9@gmail.com>
2024-10-08 11:03:50 +0900

    3.0 Introduction to Testing in Nest: 어떻게 nestjs앱을 테스트하는지에 대한 개요, pacakage.json에 테스팅에 관련한 파일이 자동 세팅되어있음. unit테스트와 e2e테스트, unit은 하나의 함수단위로 테스트를 진행하고 e2e는 하나의 서비스를 중심으로 테스트를진행함

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -15,6 +15,8 @@ export class MoviesController {
     // 하지만 이렇게 객체에 직접접근하는게 좋은 동작은 아님, 왜냐하면 nest는 express위에서 돌아갈 수도있고 fastify에서 돌아갈 수도 있기 때문.
     @Get()
     getAll(@Req() req, @Res() res):Movie[]{
+        // 이러한 @Req를 사용한 request객체에 직접접근은 좋은 동작이 아님, fastify로 바꾸면 이게 고장남.
+        // fastify가 express보다 두배정도 빠르므로 이것도 챌린지 요소로 고려할만함.
         return this.moviesService.getAll();
     }
     // search같은 하위 주소를 직접 지정하는 요청이 /:id
